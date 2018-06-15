ff237c9b05eef892fee59e3d21a26a74ecf672a0
sne <snehishdasari@gmail.com>
2018-06-15 13:03:10 +0530

    same error continues

---
+++ app/src/main/java/com/example/sne/olcademy/Category.java
@@ -0,0 +1,45 @@
+package com.example.sne.olcademy ;
+public class Category {
+
+
+    private int id ;
+    private int image ;
+    private String name ;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(int id, int image, String name)
+
+    {
+        this.id=id ;
+        this.image=image ;
+        this.name=name ;
+
+    }
+
+    //setter,getter
+
+}
--- app/src/main/java/com/example/sne/olcademy/ListPage.java
+++ app/src/main/java/com/example/sne/olcademy/ListPage.java
@@ -2,13 +2,10 @@ package com.example.sne.olcademy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListPage extends AppCompatActivity {
 
@@ -16,92 +13,29 @@ public class ListPage extends AppCompatActivity {
     } ;
 
     String[] NAMES = { "Electronics","TV & Appliances","Fashion","Home & Furniture" , "Sports"} ;
-
-
+    ListView listView =(ListView)findViewById(R.id.name_list) ;
+    private List<Category> mCategoryList ;
+    private com.example.sne.olcademy.CategoryListAdapter adapter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_page);
 
+        mCategoryList= new ArrayList<Category>() ;
 
-        ListView listView =(ListView)findViewById(R.id.name_list) ;
+        mCategoryList.add(new Category(1,R.drawable.electronics,"Electronics")) ;
+        mCategoryList.add(new Category(2,R.drawable.tvappliances,"TV & Appliances")) ;
+        mCategoryList.add(new Category(3,R.drawable.fashion,"Fashion")) ;
+        mCategoryList.add(new Category(4,R.drawable.homefurniture,"Home & Furniture")) ;
+        mCategoryList.add(new Category(5,R.drawable.sports,"Sports")) ;
 
-        CustomAdapter customAdapter = new CustomAdapter() ;
 
-        listView.setAdapter(customAdapter);
-    }
+        adapter= new com.example.sne.olcademy.CategoryListAdapter(getApplicationContext(),mCategoryList) ;
+        listView.setAdapter(adapter);
 
-    public class CustomAdapter extends BaseAdapter
-    {
-
-        public CustomAdapter()
-        {
-            super();
-        }
-        /**
-         * How many items are in the data set represented by this Adapter.
-         *
-         * @return Count of items.
-         */
-        @Override
-        public int getCount() {
-            return IMAGES.length;
-        }
-
-        /**
-         * Get the data item associated with the specified position in the data set.
-         *
-         * @param position Position of the item whose data we want within the adapter's
-         *                 data set.
-         * @return The data at the specified position.
-         */
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        /**
-         * Get the row id associated with the specified position in the list.
-         *
-         * @param position The position of the item within the adapter's data set whose row id we want.
-         * @return The id of the item at the specified position.
-         */
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        /**
-         * Get a View that displays the data at the specified position in the data set. You can either
-         * create a View manually or inflate it from an XML layout file. When the View is inflated, the
-         * parent View (GridView, ListView...) will apply default layout parameters unless you use
-         *
-         * to specify a root view and to prevent attachment to the root.
-         *
-         * @param position    The position of the item within the adapter's data set of the item whose view
-         *                    we want.
-         * @param convertView The old view to reuse, if possible. Note: You should check that this view
-         *                    is non-null and of an appropriate type before using. If it is not possible to convert
-         *                    this view to display the correct data, this method can create a new view.
-         *                    Heterogeneous lists can specify their number of view types, so that this View is
-         *                    always of the right type (see  and
-         *                    ).
-         * @param parent      The parent that this view will eventually be attached to
-         * @return A View corresponding to the data at the specified position.
-         */
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View examp=convertView ;
-            if(examp==null) {
-                examp = LayoutInflater.from(getApplicationContext()).inflate(R.layout.list1,parent,false)  ;           }
-            ImageView imageView=(ImageView)findViewById(R.id.icons) ;
-            TextView textView=(TextView)findViewById(R.id.names1) ;
-
-            imageView.setImageResource(IMAGES[position]);
-            textView.setText(NAMES[position]);
-
-            return examp;
-        }
 
     }
-}
+
+
+    }
+
